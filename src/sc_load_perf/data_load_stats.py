f8b91eb20bc1d5a65bf6b833ea93b634cda1d5e3
jr <jhanarato@gmail.com>
2025-04-10 18:45:38 +0800

    Rename columns and filter out All done stage.

--- src/sc_load_perf/data_load_stats.py
+++ src/sc_load_perf/data_load_stats.py
@@ -1,4 +1,8 @@
 import polars as pl
 
 def stats() -> pl.DataFrame:
-    return pl.read_csv('~/Code/suttacentral/server/load-data-run.csv')
+    df = pl.read_csv(
+        source='~/Code/suttacentral/server/load-data-run.csv',
+        new_columns=['number', 'message', 'clock_time_s', 'cpu_time_s']
+    )
+    return df.filter(pl.col('message') != 'All done')
